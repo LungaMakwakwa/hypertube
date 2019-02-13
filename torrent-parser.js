64b440402b8e16e1b4b5d1ca9e45f1649a96a360
Lunga MAKWAKWA <lmakwakw@e4r10p4.wethinkcode.co.za>
2019-02-13 13:04:13 +0200

    bittorrent client in node.js

--- torrent-parser.js
+++ torrent-parser.js
@@ -2,13 +2,25 @@
 
 const fs = require('fs');
 const bencode = require('bencode');
+const bignum = require('bignum');
 
+//open torrent file 
 module.exports.open = (filepath) => {
   return bencode.decode(fs.readFileSync(filepath));
 };
 
+
+//calculate torrent size
 module.exports.size = torrent => {
-  // ...
+    const size = torrent.info.files ?
+    //for torrents with one file
+    torrent.info.files.map(file => file.length).reduce((a, b) => a + b) :
+    //for torrents with multiple files
+    torrent.info.length;
+    
+    //Call installed bignum node module command is: npm install –save bignum.
+    //This is for file sizes larger than 32-bit.
+    return bignum.toBuffer(size, {size: 8});
 };
 
 module.exports.infoHash = torrent => {
